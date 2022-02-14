9c42183f5af3b15ff0fe520918b40286222a3262
farelion <marcinczownicki@gmail.com>
2022-02-14 17:32:19 +0100

    a

--- src/components/parse-data.jsx
+++ src/components/parse-data.jsx
@@ -36,55 +36,63 @@ const ParseData = () => {
             fileReader.readAsText(fileToLoad, "UTF-8");
         }
         console.log(dataObject)
+
+
+        const connectedData = [dataObject]
+        console.log(connectedData)
+
+
+        if (connectedData){
+            const unique = [...new Set(
+                connectedData.map(item =>
+                     item.trackName 
+                )
+            )];
+        
+            let mappedData = []
+            let startDate = new Date();
+            let endDate = new Date(0);
+        
+            unique.map((uniqueData) => {
+                        let timeTemp = 0
+                        let timesPlayedTemp = 0;
+                        let artistName = '';
+        
+                        connectedData.filter(connectedData => connectedData.trackName === uniqueData)
+                            .map((connectedData) => {
+                                timeTemp += connectedData.msPlayed;
+                                timesPlayedTemp += 1;
+                                artistName = connectedData.artistName;
+        
+                                let itemDate = new Date(connectedData.endTime);
+                                if (itemDate < startDate){
+                                    startDate = itemDate;
+                                }
+                                if (itemDate > endDate){
+                                    endDate = itemDate;
+                                }
+                            })
+        
+                        let dataObject = 
+                            {
+                                artistName: artistName,
+                                trackName: uniqueData,
+                                totalTime: (timeTemp / 60000).toFixed(2),
+                                timesPlayed: timesPlayedTemp,
+                            }
+                        mappedData.push(dataObject)
+                    })
+                    
+            let sortedData = [...mappedData].sort((a, b) => b.totalTime - a.totalTime)
+            setSortedData(sortedData);
+            setStartDate(startDate);
+            setEndDate(endDate);
+        }
+
+
     };
 
     useEffect(() => {
-        const connectedData = [dataObject]
-    
-        const unique = [...new Set(
-            connectedData.map(item =>
-                 item.trackName 
-            )
-        )];
-               
-        let mappedData = []
-        let startDate = new Date();
-        let endDate = new Date(0);
-    
-        unique.map((uniqueData) => {
-                    let timeTemp = 0
-                    let timesPlayedTemp = 0;
-                    let artistName = '';
-    
-                    connectedData.filter(connectedData => connectedData.trackName === uniqueData)
-                        .map((connectedData) => {
-                            timeTemp += connectedData.msPlayed;
-                            timesPlayedTemp += 1;
-                            artistName = connectedData.artistName;
-    
-                            let itemDate = new Date(connectedData.endTime);
-                            if (itemDate < startDate){
-                                startDate = itemDate;
-                            }
-                            if (itemDate > endDate){
-                                endDate = itemDate;
-                            }
-                        })
-    
-                    let dataObject = 
-                        {
-                            artistName: artistName,
-                            trackName: uniqueData,
-                            totalTime: (timeTemp / 60000).toFixed(2),
-                            timesPlayed: timesPlayedTemp,
-                        }
-                    mappedData.push(dataObject)
-                })
-                
-        let sortedData = [...mappedData].sort((a, b) => b.totalTime - a.totalTime)
-        setSortedData(sortedData);
-        setStartDate(startDate);
-        setEndDate(endDate);
     }, [])
 
     for (let i = 0; i < dataObject.length; i++) {
@@ -113,13 +121,10 @@ const ParseData = () => {
         <div className="main">
             <input type="file" name="filefield" multiple="multiple" onChange={handleChange} />
     
-            <div className="dates">
-                <div className="dates_start">From: { startDate.toDateString('DD-MM-YYYY') } </div>
-                <div className="dates_end">To: { endDate.toDateString() }</div> 
-                </div>
+
             <div className="list__wrapper">
                 {
-                    sortedData.map((item,i) => 
+                    sortedData?.map((item,i) => 
                         <div className="list__item" key={ i }>
                             <p className="number">#{ i+1 }</p>
                             <p className="artistName">Artist name: { item.artistName }</p>
